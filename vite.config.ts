3e5a5e157079132c7941145fe31f1755d80d58e3
Roman Ohiienko <ohiienko.r@gmail.com>
2024-06-12 00:43:42 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteBasicSslPlugin()],
+  build: {
+    outDir: "./docs",
+  },
+  base: "./",
+});
