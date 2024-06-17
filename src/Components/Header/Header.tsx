02d44b832bb62f58b95425b4a473a14682c11454
Roman Ohiienko <ohiienko.r@gmail.com>
2024-06-17 15:02:05 +0300

    chore: test init data in production

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,16 +2,20 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "@/Hooks";
 import classes from "./header.module.scss";
+import WebApp from "@twa-dev/sdk";
 
 const Header: FC = () => {
   const { t } = useTranslation();
   const query = useQuery();
+
+  const user = WebApp.initDataUnsafe.user;
 
   const displayName = query.get("first_name") ?? "User Name";
 
   return (
     <header className={classes.heading}>
       {t("greeting") + " " + displayName}
+      {user?.first_name}
     </header>
   );
 };
