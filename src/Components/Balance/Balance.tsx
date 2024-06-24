9044a7ab8da8716026b73898b86f05a2d639a92e
Roman Ohiienko <ohiienko.r@gmail.com>
2024-06-25 02:07:33 +0300

    fix: useEverydayBonus hook. Migrated to comparing milliseconds. Removed extra state.

--- src/Components/Balance/Balance.tsx
+++ src/Components/Balance/Balance.tsx
@@ -1,23 +1,13 @@
 import { FC } from "react";
-import {
-  Modal,
-  Headline,
-  Text,
-  Button,
-  Spinner,
-} from "@telegram-apps/telegram-ui";
+import { Modal, Headline, Text, Spinner } from "@telegram-apps/telegram-ui";
 import { useBalance } from "@/Contexts";
 import { useEverydayBonus } from "@/Hooks";
 import { BalanceCaption } from "@/Components";
 import "./styles.scss";
 
 const Balance: FC = () => {
-  const { balance, updateBalance } = useBalance();
+  const { balance } = useBalance();
   const { bonusAvailable } = useEverydayBonus();
-
-  const handleBalanceUpdate = async () => {
-    await updateBalance(3);
-  };
 
   return (
     <div className="balance">
@@ -29,22 +19,14 @@ const Balance: FC = () => {
       <Modal
         open={bonusAvailable}
         header={<Modal.Header />}
-        className="drawer-modal"
+        className="drawer-modal balance__daily-bonus"
       >
         <Headline plain={true} weight="1">
           Your daily bonus is here!
         </Headline>
-        <Text>Hey there and welcome back! Here is your 3 🌕 magic coins.</Text>
-        <Modal.Close>
-          <Button
-            mode="bezeled"
-            size="m"
-            stretched
-            onClick={handleBalanceUpdate}
-          >
-            Got it!
-          </Button>
-        </Modal.Close>
+        <Text>Hey there and welcome back!</Text>
+        <br />
+        <Text>Here are your 3 🌕 magic coins.</Text>
       </Modal>
     </div>
   );
--- src/Hooks/useEverydayBonus/useEverydayBonus.tsx
+++ src/Hooks/useEverydayBonus/useEverydayBonus.tsx
@@ -1,59 +1,50 @@
 import { useState, useEffect } from "react";
+import { useBalance } from "@/Contexts";
 import { cloudStorage } from "@/helpers";
 
 const useEverydayBonus = () => {
-  const [lastLogIn, setLastLogIn] = useState<Date | null>(null);
+  const { updateBalance } = useBalance();
   const [bonusAvailable, setBonusAvailable] = useState<boolean>(false);
-  const today = new Date();
 
   const getLastLogIn = async () => {
+    const today = new Date();
     const lastLogDate = await cloudStorage.get("last_login");
 
     if (!lastLogDate) {
-      setLastLogIn(today);
       await cloudStorage.set("last_login", today.toISOString());
     } else {
-      setLastLogIn(new Date(lastLogDate));
+      compareDates(lastLogDate, today);
     }
   };
 
-  const compareDates = async () => {
-    if (lastLogIn) {
-      const lastLoginDate = new Date(lastLogIn);
-      const lastLoginYear = lastLoginDate.getFullYear();
-      const lastLoginMonth = lastLoginDate.getMonth();
-      const lastLoginDay = lastLoginDate.getDate();
+  const compareDates = async (lastLogInDate: string, today: Date) => {
+    const lastLoginDate = new Date(lastLogInDate);
 
-      const todayYear = today.getFullYear();
-      const todayMonth = today.getMonth();
-      const todayDay = today.getDate();
+    lastLoginDate.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
 
-      const isDifferentDay =
-        todayYear > lastLoginYear ||
-        (todayYear === lastLoginYear && todayMonth > lastLoginMonth) ||
-        (todayYear === lastLoginYear &&
-          todayMonth === lastLoginMonth &&
-          todayDay > lastLoginDay);
+    const isDifferentDay = today.getTime() > lastLoginDate.getTime();
 
-      if (isDifferentDay) {
-        setBonusAvailable(true);
-        setLastLogIn(today);
-        await cloudStorage.set("last_login", today.toISOString());
-      } else {
-        setBonusAvailable(false);
+    if (isDifferentDay) {
+      setBonusAvailable(true);
+      updateBalance(3);
+      await cloudStorage.set("last_login", today.toISOString());
+    } else {
+      setBonusAvailable(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchLastLogIn = async () => {
+      try {
+        await getLastLogIn();
+      } catch (error) {
+        console.error("Failed to fetch last login date:", error);
       }
-    }
-  };
+    };
 
-  useEffect(() => {
-    getLastLogIn();
+    fetchLastLogIn();
   }, []);
-
-  useEffect(() => {
-    if (lastLogIn !== null) {
-      compareDates();
-    }
-  }, [lastLogIn]);
 
   return { bonusAvailable };
 };
