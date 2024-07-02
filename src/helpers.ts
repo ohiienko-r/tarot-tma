19b05eb6b6ebcff79c1caa89363546c23f973ac2
Roman Ohiienko <ohiienko.r@gmail.com>
2024-07-02 20:23:42 +0300

    feat: add initData validation before request to api server

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,30 +4,37 @@ import { ValidationResponse } from "./types";
 export const validateInitData = async (): Promise<boolean> => {
   const { initDataRaw } = retrieveLaunchParams();
 
-  const response = await fetch(import.meta.env.VITE_INIT_DATA_VALIDATION_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `tma ${initDataRaw}`,
-    },
-  });
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_INIT_DATA_VALIDATION_URL,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `tma ${initDataRaw}`,
+        },
+      }
+    );
 
-  const data: ValidationResponse = await response.json();
+    const data: ValidationResponse = await response.json();
 
-  return data.success;
+    return data.success;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
 };
 
 export const SPREADS = {
   CARD_OF_THE_DAY: {
-    PRICE: 1,
+    PRICE: 2,
     CARDS_QTY: 1,
   },
   YES_NO: {
-    PRICE: 1,
+    PRICE: 3,
     CARDS_QTY: 1,
   },
   QUESTION_TO_THE_CARDS: {
-    PRICE: 3,
+    PRICE: 5,
     CARDS_QTY: 3,
   },
 } as const;
