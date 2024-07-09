c455a56b5c50f2686886f2d89039998550950732
Roman Ohiienko <ohiienko.r@gmail.com>
2024-07-09 16:48:03 +0300

    fix: share text formatting

--- src/Hooks/useShareBotUrl/useShareBotUrl.tsx
+++ src/Hooks/useShareBotUrl/useShareBotUrl.tsx
@@ -1,14 +1,18 @@
 import { useCallback } from "react";
 import { useUtils } from "@telegram-apps/sdk-react";
 import { useTranslation } from "react-i18next";
+import { shareMessage } from "./helpers";
 
 const useShareBotUrl = () => {
   const utils = useUtils();
-  const { t } = useTranslation();
+  const { i18n } = useTranslation();
 
   const shareBotUrl = useCallback(() => {
-    utils.shareURL("https://t.me/my_ai_tarot_bot", t("share bot message text"));
-  }, [utils, t]);
+    utils.shareURL(
+      "https://t.me/my_ai_tarot_bot",
+      shareMessage[i18n.language] ?? shareMessage.english
+    );
+  }, [utils, i18n]);
 
   return shareBotUrl;
 };
