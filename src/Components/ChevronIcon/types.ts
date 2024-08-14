5a09515eaba545397741e7a6c9c60471b665d482
Roman Ohiienko <ohiienko.r@gmail.com>
2024-08-14 15:27:13 +0300

    feat: move all directly used svgs to separate components

---
+++ src/Components/ChevronIcon/types.ts
@@ -0,0 +1,3 @@
+export type ChevronIconPropTypes = {
+  stroke?: string;
+};
--- src/Components/Settings/Settings.tsx
+++ src/Components/Settings/Settings.tsx
@@ -6,6 +6,7 @@ import {
   useHapticFeedback,
   useCloudStorage,
 } from "@telegram-apps/sdk-react";
+import { CloseIcon, ChevronIcon } from "@/Components";
 import {
   IconButton,
   Modal,
@@ -77,20 +78,7 @@ const Settings: FC = () => {
         <Modal.Header
           after={
             <IconButton mode="plain" size="m" onClick={handleSettingsClose}>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4.50552 4.4401C4.85466 4.08631 5.4245 4.08253 5.77829 4.43166L12.0036 10.575L18.229 4.43166C18.5828 4.08253 19.1526 4.08631 19.5018 4.4401C19.8509 4.79389 19.8471 5.36373 19.4933 5.71286L13.285 11.8395L19.4933 17.9661C19.8471 18.3152 19.8509 18.8851 19.5018 19.2389C19.1526 19.5927 18.5828 19.5964 18.229 19.2473L12.0036 13.1039L5.77829 19.2473C5.4245 19.5964 4.85466 19.5927 4.50552 19.2389C4.15639 18.8851 4.16017 18.3152 4.51396 17.9661L10.7223 11.8395L4.51396 5.71286C4.16017 5.36373 4.15639 4.79389 4.50552 4.4401Z"
-                  fill="var(--tg-theme-link-color)"
-                />
-              </svg>
+              <CloseIcon />
             </IconButton>
           }
         />
@@ -102,25 +90,9 @@ const Settings: FC = () => {
           mode="plain"
           stretched
           onClick={handleChangeLanguageOpen}
-          after={
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6 3L11 8L6 13"
-                stroke="var(--tg-theme-link-color)"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          }
+          after={<ChevronIcon />}
         >
-          {`${t("language")} ${
+          {`${t("language")}: ${
             countriesFlags[i18n.language as SystemLanguage]
           }`}
         </Button>
@@ -144,7 +116,7 @@ const Settings: FC = () => {
             {t("select language")}
           </Headline>
           <Button
-            mode="outline"
+            mode="bezeled"
             stretched
             onClick={() => {
               handleChangeLanguage("en");
@@ -154,7 +126,7 @@ const Settings: FC = () => {
           </Button>
           <Divider />
           <Button
-            mode="outline"
+            mode="bezeled"
             stretched
             onClick={() => {
               handleChangeLanguage("uk");
@@ -164,7 +136,7 @@ const Settings: FC = () => {
           </Button>
           <Divider />
           <Button
-            mode="outline"
+            mode="bezeled"
             stretched
             onClick={() => {
               handleChangeLanguage("ru");
