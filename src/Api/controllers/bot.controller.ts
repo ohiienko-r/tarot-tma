0556fea883b0c4a4ea7b746163b2863cd88fe2d6
Roman Ohiienko <ohiienko.r@gmail.com>
2024-09-05 17:05:57 +0300

    chore: restore Api folder

---
+++ src/Api/controllers/bot.controller.ts
@@ -0,0 +1,119 @@
+import { initDataRaw } from "@/Telegram";
+import {
+  FeedbackBody,
+  SendSpreadToUserBody,
+  ValidationResponse,
+} from "@/types";
+
+export default {
+  /**
+   * Validates user's Telegram init data to verify that user is the exact person;
+   * @returns true if user's Telegram init data is valid, false if not;
+   */
+  async validateInitData(): Promise<boolean> {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_INIT_DATA_VALIDATION_URL,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `tma ${initDataRaw}`,
+          },
+        }
+      );
+
+      const data: ValidationResponse = await response.json();
+
+      return data.success;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  },
+  /**
+   *
+   * @param {string} title - invoice title;
+   * @param {string} description - invoice description;
+   * @param {number} amount - invoice price to be charged;
+   * @param {number} [coinsQty] - (Optional) Quantity of Magic Coins to be added to user's balance in case of successfull transaction
+   * @returns Telegram's invoice presented as URL;
+   */
+  async getInvoiceLink(
+    title: string,
+    description: string,
+    amount: number,
+    coinsQty?: number
+  ): Promise<string | undefined> {
+    try {
+      const invoiceTitle = coinsQty ? `${coinsQty} ${title}` : title;
+
+      const response = await fetch(
+        "https://tarot-bot-18921c9756be.herokuapp.com/create_invoice",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: invoiceTitle,
+            description: description,
+            amount: amount,
+            label: title,
+          }),
+        }
+      );
+
+      const invoiceLink = await response.json();
+
+      return invoiceLink;
+    } catch (error) {
+      console.error(`An error occured: ${error}`);
+    }
+  },
+  /**
+   * Sends a user's feedback to Bot api server where it's sent to a DB and to admin's personal chat with Bot;
+   * @param {FeedbackBody} body - object that contains user's Tg id, first name, rating of app from 1 to 5 and feedback string;
+   */
+  async sendFeedback(body: FeedbackBody) {
+    try {
+      await fetch(import.meta.env.VITE_SEND_FEEDBACK_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      console.error("Failed to send feedback to server:", error);
+    }
+  },
+  /**
+   *  Sends a user's whole spread reading and cards keys to Bot API server where it's sent into a private chat with bot to a
+   * respective user accirding to uId;
+   * @param {SendSpreadToUserBody} body - object that contains user's Tg id, array of cards object keys, spread title,
+   * user's prompt (if applicable) and LLM response Tarot readings string;
+   */
+  async sendSpreadToUser({
+    uId,
+    cardsKeys,
+    title,
+    prompt,
+    reading,
+  }: SendSpreadToUserBody) {
+    try {
+      await fetch(import.meta.env.VITE_SEND_SPREAD_TO_USER_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          uId: uId,
+          cardsKeys: cardsKeys,
+          prompt: prompt,
+          title: title,
+          reading: reading,
+        }),
+      });
+    } catch (error) {
+      console.error("Failed to send spread to user:", error);
+    }
+  },
+};
---
+++ src/Api/controllers/readings.controller.ts
@@ -0,0 +1,131 @@
+import { ROUTES_NAMES } from "@/Router";
+import { prompts } from "../api.dto";
+import { Card, SystemLanguage, Path } from "@/types";
+
+/**
+ *
+ * @param {Card[]} card - one of the Tarot cards names;
+ * @param {SystemLanguage} systemLanguage - IETF Language code supported by app;
+ * @returns LLM response Tarot readings string for the provided card;
+ */
+const getCardOfTheDayReading = async (
+  card: Card[],
+  systemLanguage: SystemLanguage
+): Promise<string | null> => {
+  try {
+    const response = await fetch(import.meta.env.VITE_CARD_OF_THE_DAY_SPREAD, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        language: systemLanguage,
+        cards: card,
+        prompt: prompts[systemLanguage].cardOfTheDay,
+      }),
+    });
+
+    const responseText = await response.text();
+    return responseText;
+  } catch (error) {
+    throw new Error(
+      `Failed to get a card of the days readings response: ${error}`
+    );
+  }
+};
+
+/**
+ *
+ * @param {Card[]} card - one of the Tarot cards names;
+ * @param {SystemLanguage} systemLanguage - IETF Language code supported by app;
+ * @returns LLM response Tarot readings string for the provided card;
+ */
+const getYesNoReading = async (
+  card: Card[],
+  systemLanguage: SystemLanguage
+): Promise<string | null> => {
+  try {
+    const response = await fetch(import.meta.env.VITE_CARD_OF_THE_DAY_SPREAD, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        language: systemLanguage,
+        cards: card,
+        prompt: prompts[systemLanguage].yesNo,
+      }),
+    });
+
+    const responseText = await response.text();
+    return responseText;
+  } catch (error) {
+    throw new Error(`Failed to get a yes/no readings response: ${error}`);
+  }
+};
+
+/**
+ *
+ * @param {Card[]} cards - three Tarot cards names;
+ * @param {SystemLanguage} systemLanguage - IETF Language code supported by app;
+ * @param {string} prompt - user's prompt to LLM represented by so called question to the cards;
+ * @returns LLM response Tarot readings string for the provided cards;
+ */
+const getQuestionReading = async (
+  cards: Card[],
+  systemLanguage: SystemLanguage,
+  prompt: string
+): Promise<string | null> => {
+  try {
+    const response = await fetch(import.meta.env.VITE_CARD_OF_THE_DAY_SPREAD, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        language: systemLanguage,
+        cards: cards,
+        prompt: prompt,
+      }),
+    });
+
+    const responseText = await response.text();
+    return responseText;
+  } catch (error) {
+    throw new Error(
+      `Failed to get a question to the cards readings response: ${error}`
+    );
+  }
+};
+
+/**
+ * Controller function for calling a function that gets required type of readings according to router path;
+ * @param {Card[]} card - one or three Tarot cards names;
+ * @param {SystemLanguage} systemLanguage - IETF Language code supported by app;
+ * @param {Path} path - router navigation path;
+ * @param {string} prompt - user's prompt to LLM represented by so called question to the cards;
+ * @returns LLM response Tarot readings string for the provided cards;
+ */
+const getReadings = async (
+  card: Card[],
+  systemLanguage: SystemLanguage,
+  path: Path,
+  prompt?: string
+) => {
+  switch (path) {
+    case ROUTES_NAMES.CARD_OF_THE_DAY:
+      return await getCardOfTheDayReading(card, systemLanguage);
+    case ROUTES_NAMES.YES_NO:
+      return await getYesNoReading(card, systemLanguage);
+    case ROUTES_NAMES.QUESTION_INPUT:
+      return await getQuestionReading(
+        card,
+        systemLanguage,
+        prompt ?? prompts[systemLanguage].fallBackPrompt
+      );
+    default:
+      return "Please provide the correct path";
+  }
+};
+
+export default { getReadings };
